adcfdb970a7158c180b78aa4c3517f6be856a2d0
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-10-04 11:22:50 +0200

    Start implementing `tricontour` plot

--- examples/kinematics/plot_operational_workspace.py
+++ examples/kinematics/plot_operational_workspace.py
@@ -3,6 +3,7 @@ from jax import config as jax_config
 
 jax_config.update("jax_enable_x64", True)  # double precision
 jax_config.update("jax_platform_name", "cpu")  # use CPU
+import jax.numpy as jnp
 import matplotlib
 
 matplotlib.use("Qt5Cairo")
@@ -34,6 +35,28 @@ if __name__ == "__main__":
 
     max_actuation_samples = data["max_actuation"]
     min_actuation_samples = data["min_actuation"]
+    random_samples = data["random"]
+
+    fig = plt.figure(figsize=(4.5, 4.5), num=f"Operational workspace of {hsa_material} HSA")
+
+    tricontour_x = random_samples["chiee_ss"][:, 0] * 1e3
+    tricontour_y = random_samples["chiee_ss"][:, 1] * 1e3
+    # effort (i.e. the color) is the mean actuation magnitude
+    tricontour_z = jnp.mean(random_samples["phi_ss"], axis=-1)
+    plt.tricontour(
+        tricontour_x,
+        tricontour_y,
+        tricontour_z,
+        1000,
+        cmap="viridis",
+    )
+    plt.plot(
+        tricontour_x,
+        tricontour_y,
+        linestyle="None",
+        marker=".",
+    )
+
     plt.plot(
         max_actuation_samples["chiee_ss"][:, 0] * 1e3,
         max_actuation_samples["chiee_ss"][:, 1] * 1e3,
@@ -46,6 +69,7 @@ if __name__ == "__main__":
         "k--",
         label="Min. actuation"
     )
+
     plt.xlabel(r"$p_{\mathrm{ee},x}$ [mm]")
     plt.ylabel(r"$p_{\mathrm{ee},y}$ [mm]")
     plt.axis("equal")
