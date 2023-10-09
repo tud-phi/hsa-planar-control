7962b7753d056ac98ca60041ecd4dc2f4b44532a
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-10-09 14:12:51 +0000

    Add random setpoints node (no planning, but based on rollout)

---
+++ hsa_planar_control/ros_nodes/random_setpoints_node.py
@@ -0,0 +1,188 @@
+from ament_index_python.packages import get_package_share_directory
+import derivative
+from example_interfaces.msg import Float64MultiArray
+from functools import partial
+from jax import config as jax_config
+
+jax_config.update("jax_enable_x64", True)  # double precision
+jax_config.update("jax_platform_name", "cpu")  # use CPU
+from jax import Array, jit, random
+from jax import numpy as jnp
+import jsrm
+from jsrm.parameters.hsa_params import PARAMS_FPU_CONTROL, PARAMS_EPU_CONTROL
+from jsrm.systems import planar_hsa
+import rclpy
+from rclpy.node import Node
+from rclpy.time import Time
+from pathlib import Path
+import os
+
+from hsa_control_interfaces.msg import PlanarSetpoint
+from hsa_planar_control.simulation import simulate_steady_state
+from mocap_optitrack_interfaces.msg import PlanarCsConfiguration
+
+
+class RandonSetpointsNode(Node):
+    def __init__(self):
+        super().__init__("random_setpoints_node")
+
+        # set random seed
+        self.rng = random.PRNGKey(seed=0)
+
+        self.declare_parameter("setpoint_topic", "setpoint")
+        self.pub = self.create_publisher(
+            PlanarSetpoint, self.get_parameter("setpoint_topic").value, 10
+        )
+
+        # filepath to symbolic expressions
+        sym_exp_filepath = (
+            Path(jsrm.__file__).parent
+            / "symbolic_expressions"
+            / f"planar_hsa_ns-1_nrs-2.dill"
+        )
+        (
+            forward_kinematics_virtual_backbone_fn,
+            forward_kinematics_end_effector_fn,
+            jacobian_end_effector_fn,
+            inverse_kinematics_end_effector_fn,
+            dynamical_matrices_fn,
+            sys_helpers,
+        ) = planar_hsa.factory(sym_exp_filepath)
+
+        self.declare_parameter("hsa_material", "fpu")
+        hsa_material = self.get_parameter("hsa_material").value
+        if hsa_material == "fpu":
+            self.params = PARAMS_FPU_CONTROL.copy()
+        elif hsa_material == "epu":
+            self.params = PARAMS_EPU_CONTROL.copy()
+        else:
+            raise ValueError(f"Unknown HSA material: {hsa_material}")
+
+        # parameters for specifying different rest strains
+        self.declare_parameter("kappa_b_eq", self.params["kappa_b_eq"].mean().item())
+        self.declare_parameter("sigma_sh_eq", self.params["sigma_sh_eq"].mean().item())
+        self.declare_parameter("sigma_a_eq", self.params["sigma_a_eq"].mean().item())
+        kappa_b_eq = self.get_parameter("kappa_b_eq").value
+        sigma_sh_eq = self.get_parameter("sigma_sh_eq").value
+        sigma_a_eq = self.get_parameter("sigma_a_eq").value
+        self.params["kappa_b_eq"] = kappa_b_eq * jnp.ones_like(
+            self.params["kappa_b_eq"]
+        )
+        self.params["sigma_sh_eq"] = sigma_sh_eq * jnp.ones_like(
+            self.params["sigma_sh_eq"]
+        )
+        self.params["sigma_a_eq"] = sigma_a_eq * jnp.ones_like(
+            self.params["sigma_a_eq"]
+        )
+        # external payload mass (assumed to be at end effector)
+        self.declare_parameter("payload_mass", 0.0)
+        self.params["mpl"] = self.get_parameter("payload_mass").value
+
+        self.declare_parameter("phi_max", self.params["phi_max"].mean().item())
+        self.params["phi_max"] = self.get_parameter("phi_max").value * jnp.ones_like(
+            self.params["phi_max"]
+        )
+
+        # specify a jitted version of the forward kinematics function
+        self.forward_kinematics_end_effector_fn = jit(
+            partial(forward_kinematics_end_effector_fn, self.params)
+        )
+
+        # define residual function for static inversion optimization
+        sim_dt = 1e-3  # time step for simulation [s]
+        duration = 20.0  # duration of simulation [s]
+        self.simulate_steady_state_fn = jit(
+            partial(
+                simulate_steady_state,
+                dynamical_matrices_fn,
+                self.params,
+                sim_dt=sim_dt,
+                duration=duration
+            )
+        )
+
+        # jit the forward kinematics and steady state simulation function
+        phi_ss_dummy = self.params["phi_max"].flatten()
+        q_ss_dummy, q_d_ss_dummy = self.simulate_steady_state_fn(phi_ss_dummy)
+        assert q_d_ss_dummy.mean() < 1e-2, (
+            "The simulation hasn't converged to a steady state which means that likely the duration is too short." +
+            f"q_ss_dummy: {q_ss_dummy}, q_d_ss_dummy: {q_d_ss_dummy}"
+        )
+        chiee_ss_dummy = self.forward_kinematics_end_effector_fn(q_ss_dummy)
+        self.get_logger().info(f"Finished jitting the forward kinematics and steady state simulation function.")
+
+        # initial setpoint index
+        self.setpoint_idx = 0
+
+        self.declare_parameter("setpoint_frequency", 0.04)
+        self.timer = self.create_timer(
+            1 / self.get_parameter("setpoint_frequency").value, self.timer_callback
+        )
+
+    def timer_callback(self):
+        # split PRNG key
+        self.rng, rng_setpoint = random.split(self.rng)
+
+        # generate random actuation
+        phi_ss_lb = jnp.clip(
+            (self.params["phi_max"] * self.params["h"]).flatten(),
+            a_min=None,
+            a_max=0.0,
+        )  # lower bound for sampling
+        phi_ss_ub = jnp.clip(
+            (self.params["phi_max"] * self.params["h"]).flatten(),
+            a_min=0.0,
+            a_max=None,
+        )  # upper bound for sampling
+        phi_ss = random.uniform(
+            rng_setpoint,
+            shape=phi_ss_lb.shape,
+            minval=phi_ss_lb,
+            maxval=phi_ss_ub
+        )
+
+        rollout_start_time = self.get_clock().now()
+        q_ss, q_d_ss = self.simulate_steady_state_fn(phi_ss)
+        chiee_ss = self.forward_kinematics_end_effector_fn(q_ss)
+
+        # Log how long the rollout took
+        self.get_logger().info(
+            f"Rollout to steady-state took: {(self.get_clock().now() - rollout_start_time).nanoseconds / 1e6} ms"
+        )
+
+        # Log the setpoint
+        self.get_logger().info(
+            f"chiee_ss: {chiee_ss}, q_ss: {q_ss}, phi_ss: {phi_ss}"
+        )
+
+        msg = PlanarSetpoint()
+        msg.chiee_des.x = chiee_ss[0].item()
+        msg.chiee_des.y = chiee_ss[1].item()
+        msg.chiee_des.theta = chiee_ss[2].item()
+        msg.q_des.header.stamp = self.get_clock().now().to_msg()
+        msg.q_des.kappa_b = q_ss[0].item()
+        msg.q_des.sigma_sh = q_ss[1].item()
+        msg.q_des.sigma_a = q_ss[2].item()
+        msg.phi_ss = phi_ss.tolist()
+        self.pub.publish(msg)
+
+        self.setpoint_idx += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    print("Hi from the random setpoints node.")
+
+    node = RandonSetpointsNode()
+
+    rclpy.spin(node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -53,9 +53,10 @@ setup(
     tests_require=["codecov", "coverage", "pytest", "pytest-cov", "pytest-html", "tox"],
     entry_points={
         "console_scripts": [
-            "model_based_control_node = hsa_planar_control.ros_nodes.model_based_control_node:main",
-            "static_inversion_planning_node = hsa_planar_control.ros_nodes.static_inversion_planning_node:main",
             "calibration_node = hsa_planar_control.ros_nodes.calibration_node:main",
+            "model_based_control_node = hsa_planar_control.ros_nodes.model_based_control_node:main",
+            "random_setpoints_node = hsa_planar_control.ros_nodes.random_setpoints_node:main",
+            "static_inversion_planning_node = hsa_planar_control.ros_nodes.static_inversion_planning_node:main",
         ],
     },
 )
