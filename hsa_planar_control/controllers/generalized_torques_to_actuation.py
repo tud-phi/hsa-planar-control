0165e87a587065a789653b2af5c9b9d8e7b76460
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-10-13 22:11:51 +0200

    Add `operational_space_impedance_control_nonlinear_actuation` controller

--- hsa_planar_control/controllers/generalized_torques_to_actuation.py
+++ hsa_planar_control/controllers/generalized_torques_to_actuation.py
@@ -1,5 +1,6 @@
 from jax import Array, debug, jacfwd, jit
 import jax.numpy as jnp
+from jaxopt import GaussNewton, LevenbergMarquardt
 from typing import Callable, Tuple
 
 
@@ -46,5 +47,39 @@ def map_generalized_torques_to_actuation_with_linearized_model(
 
     # compute the desired phi
     phi_des = phi_eq + jnp.linalg.pinv(A) @ (tau_q_des - tau_eq)
+
+    return phi_des
+
+
+def map_generalized_torques_to_actuation_with_nonlinear_optimization(
+    dynamical_matrices_fn: Callable, q: Array, tau_q_des: Array, phi0: Array
+) -> Array:
+    """
+    Map a desired torque in configuration space to desired twist angles by performing nonlinear optimization
+    Args:
+        dynamical_matrices_fn: Callable that returns the B, C, G, K, D, and alpha.
+        q: configuration vector of shape (n_q, )
+        tau_q_des: desired torque in configuration space of shape (n_q, )
+        phi0: initial guess for the motor positions of shape (n_phi, )
+    Returns:
+        phi_des: desired motor positions (n_phi, )
+    """
+
+    def residual_fn(_phi: Array) -> Array:
+        _, _, _, _, _, _alpha = dynamical_matrices_fn(q, jnp.zeros_like(q), _phi)
+        return _alpha - tau_q_des
+
+    # solve the nonlinear least squares problem
+    lm = LevenbergMarquardt(residual_fun=residual_fn)
+    sol = lm.run(phi0)
+
+    # optimal phi
+    phi_des = sol.params
+
+    # compute the optimality
+    optimality = lm.optimality_fun(sol.params)
+
+    debug.print("optimality = {optimality}", optimality=optimality)
+    debug.print("sol = {sol}", sol=sol)
 
     return phi_des
